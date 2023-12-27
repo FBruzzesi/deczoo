cfa8fce451dc7c7ddbcafedda0c46ed8c5276bc4
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2023-12-27 19:49:34 +0100

    raise if decorator

---
+++ tests/decorators_test/raise_if_test.py
@@ -0,0 +1,26 @@
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
+from deczoo import raise_if
+
+
+@pytest.mark.parametrize(
+    "condition, context",
+    [
+        (lambda: True, pytest.raises(ValueError)),
+        (lambda: False, does_not_raise()),
+    ],
+)
+def raise_if_test(condition, context):
+    err_msg = "Test exception"
+
+    @raise_if(condition, exception=ValueError, message=err_msg)
+    def dummy_function():
+        pass
+
+    with context as exc_info:
+        dummy_function()
+
+    if exc_info:
+        assert err_msg in str(exc_info.value)
